99429ce0f6cdf87e8ec08cf2464789796e70dddf
sarahchang2013 <sarah_sarah99@hotmail.com>
2017-12-04 12:22:34 +0800

    code frame for CUD without test

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -11,12 +11,14 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 @app.route('/')
 @app.route('/index')
 def homePage():
 	categories = session.query(Category).all()
 	latestItems = session.query(Item).order_by(Item.id.desc()).limit(10)
 	return render_template("index.html", categories=categories, latestItems=latestItems)
+
 
 @app.route('/catalog/<name>/items')
 def categoryDetails(name):
@@ -25,6 +27,7 @@ def categoryDetails(name):
 	items = category.items
 	count = len(items)
 	return render_template("categoryDetails.html", categories=categories, category_name=name, items=items, count=count)
+
 
 @app.route('/catalog/<category_name>/<item_title>')
 def itemDescription(category_name, item_title):
@@ -36,6 +39,47 @@ def itemDescription(category_name, item_title):
 def catalogJSON():
 	categories = session.query(Category).all()
 	return jsonify(Category=[c.serialize for c in categories])
+
+
+@app.route('/catalog/add', methods=['GET','POST'])
+def addItem():
+	if request.method == 'POST':
+		#Retrieve form data
+		#code here
+		newItem = Item(title=title, description=description, category_id=category_id, user_id=user_id)
+		session.add(newItem)
+		session.commit()
+		return render_template("index.html")
+	else:
+		return render_template("addItem.html")
+
+
+@app.route('/catalog/<item_title>/edit', methods=['GET','POST'])
+def editItem(item_title):
+	if request.method == 'POST':
+		editedItem = session.query(Item).filter(Item.title == item_title).one()
+		#Retrieve form data
+		#code here
+		editedItem.title = title
+		editedItem.description = description
+		editedItem.category_id = category_id
+		session.commit()
+		return render_template("itemDescription.html", item=item)
+	else:
+		return render_template("editItem.html", item_title=item_title)
+
+
+@app.route('/catalog/<item_title>/delete', methods=['GET','POST'])
+def deleteItem(item_title):
+	if request.method == 'POST':
+		deletedItem = session.query(Item).filter(Item.title == item_title).one()
+		#Retrieve form data
+		#code here
+		session.delete(deletedItem)
+		session.commit()
+		return render_template("index.html")
+	else:
+		return render_template("deleteItem.html", item_title=item_title)
 
 
 if __name__ == '__main__':
