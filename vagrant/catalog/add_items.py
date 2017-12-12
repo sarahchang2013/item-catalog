fbda2069b8ed19e200a8632a94ff64f835127306
sarahchang2013 <sarah_sarah99@hotmail.com>
2017-12-12 22:34:14 +0800

    Conform to PEP8: break long lines, replace tabs with 4 spaces

--- vagrant/catalog/add_items.py
+++ vagrant/catalog/add_items.py
@@ -9,60 +9,60 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-#add a dummy user
+# add a dummy user
 user1 = User(name="dummyUser", email="dummyuseremail")
 session.add(user1)
 session.commit()
 
-#add new categories 
-cat1 = Category(name = "Soccer")
+# add new categories
+cat1 = Category(name="Soccer")
 session.add(cat1)
 session.commit()
 
-cat2 = Category(name = "Basketball")
+cat2 = Category(name="Basketball")
 session.add(cat2)
 session.commit()
 
-cat3 = Category(name = "Baseball")
+cat3 = Category(name="Baseball")
 session.add(cat3)
 session.commit()
 
-cat4 = Category(name = "Frisbee")
+cat4 = Category(name="Frisbee")
 session.add(cat4)
 session.commit()
 
-cat5 = Category(name = "Snowboarding")
+cat5 = Category(name="Snowboarding")
 session.add(cat5)
 session.commit()
 
-cat6 = Category(name = "Rock Climbing")
+cat6 = Category(name="Rock Climbing")
 session.add(cat6)
 session.commit()
 
-cat7 = Category(name = "Skating")
+cat7 = Category(name="Skating")
 session.add(cat7)
 session.commit()
 
-cat8 = Category(name = "Hockey")
+cat8 = Category(name="Hockey")
 session.add(cat8)
 session.commit()
 
 
-#add new items in each category
-item1 = Item(user_id = 1, category_id = 1, title = "Soccer Cleats", description = "The Shoes")
+# add new items in each category
+item1 = Item(user_id=1, category_id=1, title="Soccer Cleats",
+             description="The Shoes")
 session.add(item1)
 session.commit()
 
-item2 = Item(user_id = 1, category_id = 1, title = "Jersey", description = "The Shirt")
+item2 = Item(user_id=1, category_id=1, title="Jersey", description="The Shirt")
 session.add(item2)
 session.commit()
 
-item3 = Item(user_id = 1, category_id = 3, title = "Bat", description = "The bat")
+item3 = Item(user_id=1, category_id=3, title="Bat", description="The bat")
 session.add(item3)
 session.commit()
 
-item4 = Item(user_id = 1, category_id = 5, title = "Snowboard", description = "The Snowboard")
+item4 = Item(user_id=1, category_id=5, title="Snowboard",
+             description="The Snowboard")
 session.add(item4)
 session.commit()
-
-
--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -7,58 +7,58 @@ Base = declarative_base()
 
 
 class User(Base):
-	__tablename__ = 'user'
-	
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
-	email = Column(String(250), nullable=False)
-	picture = Column(String(250))
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 
 class Category(Base):
-	__tablename__ = 'category'
+    __tablename__ = 'category'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
-	items = relationship("Item", back_populates="category")
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    items = relationship("Item", back_populates="category")
 
-	#serialize function
-	@property
-	def serialize(self):
-		"""Return object data in serialization"""
-		if len(self.items):
-			return {
-					"Item": [i.serialize for i in self.items],
-					"name": self.name,
-					"id": self.id
-					}
-		else:
-			return {
-					"name": self.name,
-					"id": self.id
-					}
+    # serialize function
+    @property
+    def serialize(self):
+        """Return object data in serialization"""
+        if len(self.items):
+            return {
+                    "Item": [i.serialize for i in self.items],
+                    "name": self.name,
+                    "id": self.id
+                   }
+        else:
+            return {
+                    "name": self.name,
+                    "id": self.id
+                   }
 
 
 class Item(Base):
-	__tablename__ = 'item'
+    __tablename__ = 'item'
 
-	id = Column(Integer, primary_key=True)
-	title = Column(String(250), nullable=False)
-	description = Column(String(800), nullable=False)
-	category_id = Column(Integer, ForeignKey('category.id'))
-	category = relationship("Category", back_populates="items")
-	user_id = Column(Integer, ForeignKey('user.id'))	
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    description = Column(String(800), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship("Category", back_populates="items")
+    user_id = Column(Integer, ForeignKey('user.id'))
 
-	#serialize function
-	@property
-	def serialize(self):
-		"""Return object data in serialization"""
-		return {
-				"cat_id": self.category_id,
-				"description": self.description,
-				"id": self.id,
-				"title": self.title
-				}
+    # serialize function
+    @property
+    def serialize(self):
+        """Return object data in serialization"""
+        return {
+                "cat_id": self.category_id,
+                "description": self.description,
+                "id": self.id,
+                "title": self.title
+               }
 
 
 engine = create_engine('sqlite:///catalog.db')
